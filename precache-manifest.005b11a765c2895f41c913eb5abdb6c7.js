618d449ef7261032e32317fee1732e30f1dbcb43
Wael Naama <46049820+WaelNaame@users.noreply.github.com>
2019-05-06 04:55:29 +0300

    Add files via upload

---
+++ precache-manifest.005b11a765c2895f41c913eb5abdb6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b585845b42d5a41ae781bd75777249b",
+    "url": "/static/media/empty-cart-img.1b585845.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ebe2987a6a547c57e53d",
+    "url": "/static/js/main.fc703dce.chunk.js"
+  },
+  {
+    "revision": "954384a5484d831857b9",
+    "url": "/static/js/2.1c1ee600.chunk.js"
+  },
+  {
+    "revision": "ebe2987a6a547c57e53d",
+    "url": "/static/css/main.84bc9150.chunk.css"
+  },
+  {
+    "revision": "9354c456e5b67f285f2b1a798689410d",
+    "url": "/index.html"
+  }
+];
